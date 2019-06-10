5a9125bc799a723c10311b1fa5c2ab55a09cb817
herteleo <leonard.hertel@gmail.com>
2019-06-10 17:51:24 +0200

    feat: general page layout

--- saber-config.js
+++ saber-config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  siteConfig: {
+    title: 'Pigment – Manage your favorite WebApps in one place and benefit from anti-stress features to stay focused',
+  },
   permalinks: {
     page: '/:slug',
     post: '/:year/:month/:day/:slug',
