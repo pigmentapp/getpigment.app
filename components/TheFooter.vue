87d21a4658de8409d5bbf142146cf8391d03ec41
herteleo <leonard.hertel@gmail.com>
2019-06-10 17:28:45 +0200

    feat: header and footer

---
+++ components/TheFooter.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav :class="$style.footer">
+    <app-container :class="$style.container">
+      Made with ❤️ by <a href="//herteleo.de">@herteleo</a>
+      &middot;
+      <a href="/legal/imprint.html">
+        Imprint &amp; Contact (Impressum &amp; Kontakt)
+      </a>
+      &middot;
+      <a href="/legal/data-protection.html">
+        Data protection (Datenschutz)
+      </a>
+    </app-container>
+  </nav>
+</template>
+
+<script>
+export default {
+  computed: {
+    year() {
+      return new Date().getFullYear();
+    },
+  },
+};
+</script>
+
+<style lang="postcss" module>
+.footer {
+  @apply flex-shrink-0 mt-auto text-gray-600;
+}
+
+.footer a:hover {
+  @apply text-gray-400;
+}
+
+.container {
+  @apply py-4 text-center;
+}
+</style>
---
+++ components/TheHeader.vue
@@ -0,0 +1,62 @@
+<template>
+  <nav :class="$style.header">
+    <app-container :class="$style.container">
+      <h1 :class="$style.brand">
+        <saber-link to="/">
+          Pigment
+        </saber-link>
+      </h1>
+      <div :class="$style.nav">
+        <nav-list :class="$style.list">
+          <nav-list-item
+            external
+            to="//github.com/pigmentapp/pigment"
+          >
+            GitHub
+          </nav-list-item>
+        </nav-list>
+        <app-button
+          to="/download"
+          primary
+        >
+          Download
+        </app-button>
+      </div>
+    </app-container>
+  </nav>
+</template>
+
+<script>
+import NavList from './NavList.vue';
+import NavListItem from './NavListItem.vue';
+
+export default {
+  components: {
+    NavList,
+    NavListItem,
+  },
+};
+</script>
+
+
+<style lang="postcss" module>
+.header {
+  @apply flex-shrink-0 border-b border-gray-800;
+}
+
+.container {
+  @apply flex items-center justify-between py-2;
+}
+
+.brand {
+  @apply font-light leading-none text-xl;
+}
+
+.nav {
+  @apply flex items-center;
+}
+
+.list {
+  @apply mr-4 uppercase;
+}
+</style>
